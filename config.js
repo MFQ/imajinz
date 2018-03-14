6359b20df0a499c6e531aa37f8069cc78a8989a4
Muhammad Fazil Qureshi <softmfq@gmail.com>
2018-03-14 18:09:52 +0500

    added node-convict categorize dependency on the basis of enviorment

---
+++ config.js
@@ -0,0 +1,39 @@
+const convict = require('convict');
+
+// Define a schema
+const config = convict({
+  env: {
+    doc: "The application environment.",
+    format: ["production", "development", "test"],
+    default: "development",
+    env: "NODE_ENV"
+  },
+  port: {
+    doc: "The port to bind.",
+    format: "port",
+    default: 8080,
+    env: "PORT",
+    arg: "port"
+  },
+  db: {
+    host: {
+      doc: "Database host name/IP",
+      format: '*',
+      default: 'server1.dev.test'
+    },
+    name: {
+      doc: "Database name",
+      format: String,
+      default: 'users'
+    }
+  }
+});
+
+// Load environment dependent configuration
+const env = config.get('env');
+config.loadFile('./config/' + env + '.json');
+
+// Perform validation
+config.validate({allowed: 'strict'});
+
+module.exports = config;
