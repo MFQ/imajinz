2b525b16c880930ba4805d8b600cc5d2feda9113
Muhammad Fazil Qureshi <softmfq@gmail.com>
2018-03-17 04:03:39 +0500

    added console.log for testing

--- test/units/db.test.js
+++ test/units/db.test.js
@@ -4,6 +4,9 @@ const sysConfig = require('./../../config.js');
 
 const dbConfig = require('./../../config/db.js')[sysConfig.get('env')];
 
+console.log('__________________');
+console.log(sysConfig.get('env'));
+
 describe('Verify database', () => {
   it('should respond to database connection', (done) => {
     const s = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
