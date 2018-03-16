75357a0dc38621599416df196682043c75389218
Muhammad Fazil Qureshi <softmfq@gmail.com>
2018-03-17 01:45:15 +0500

    test added for db configurations

---
+++ test/units/db.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const Sequelize = require('sequelize');
+const sysConfig = require('./../../config.js');
+
+const dbConfig = require('./../../config/db.js')[sysConfig.get('env')];
+
+describe('Verify database', () => {
+  it('should respond to database connection', (done) => {
+    const s = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
+    s.authenticate()
+      .then(() => {
+        assert.ok(true);
+        done();
+      })
+      .catch(() => {
+        assert.fail('unable to authenticate database');
+        done();
+      });
+  });
+});
