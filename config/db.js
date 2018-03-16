2240ca83ab457c532dca460941eddcf8e2603ed3
Muhammad Fazil Qureshi <softmfq@gmail.com>
2018-03-17 04:00:26 +0500

    updated test configs

--- config/db.js
+++ config/db.js
@@ -7,10 +7,10 @@ module.exports = {
     dialect: 'postgres',
   },
   test: {
-    username: 'root',
-    password: null,
-    database: 'imajinz_test',
-    host: '127.0.0.1',
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || 'imajinz_test',
+    host: process.env.DB_HOSTNAME || '127.0.0.1',
     dialect: 'postgres',
   },
   production: {
